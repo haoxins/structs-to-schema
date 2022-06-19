ad7079d4782212f9798a294461b2c682caa8483c
Xin Hao <haoxinst@gmail.com>
2022-06-19 19:28:16 +0800

    Add MVP Avro supports

--- avro.go
+++ avro.go
@@ -1,1 +1,89 @@
 package structstoschema
+
+import (
+	"errors"
+	"reflect"
+)
+
+const (
+	AvroTypeRecord = "record"
+	AvroTypeString = "string"
+	AvroTypeBool   = "boolean"
+	AvroTypeInt32  = "int"
+	AvroTypeInt64  = "long"
+)
+
+type Element struct {
+	Name     string
+	Type     string
+	Children []Element
+}
+
+func ParseAvro(in any) (string, error) {
+	root := Element{}
+	t := reflect.TypeOf(in)
+
+	if t.Kind() != reflect.Struct {
+		return "", errors.New("Invalid type")
+	}
+
+	root.Name = t.Name()
+	root.Type = AvroTypeRecord
+	root.Children = ParseAvroElements(t)
+
+	return root.toAvsc(), nil
+}
+
+func (e *Element) toAvsc() string {
+	s := ""
+	if len(e.Children) == 0 {
+		s = `{"name":"` + e.Name + `","type":"` + e.Type + `"}`
+	} else {
+		fields := ""
+		for _, child := range e.Children {
+			fields += "," + child.toAvsc()
+		}
+		fields = fields[1:]
+		s = `{"name":"` + e.Name + `","type":"` + e.Type + `","fields":[` + fields + `]}`
+	}
+
+	return s
+}
+
+func ParseAvroElements(t reflect.Type) []Element {
+	elements := []Element{}
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Type.Kind() != reflect.Struct {
+			elements = append(elements, Element{
+				Name: field.Name,
+				Type: castTypeToAvro(field.Type.Kind()),
+			})
+		} else {
+			elements = append(elements, Element{
+				Name:     field.Name,
+				Type:     AvroTypeRecord,
+				Children: ParseAvroElements(field.Type),
+			})
+		}
+	}
+
+	return elements
+}
+
+func castTypeToAvro(k reflect.Kind) string {
+	switch k {
+	case reflect.String:
+		return AvroTypeString
+	case reflect.Bool:
+		return AvroTypeBool
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32:
+		return AvroTypeInt32
+	case reflect.Int64:
+		return AvroTypeInt64
+	// TODO: Add more types
+	default:
+		panic(errors.New("Invalid type"))
+	}
+}
--- avro_test.go
+++ avro_test.go
@@ -1,1 +1,38 @@
 package structstoschema
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+type Event struct {
+	ClientIP  string  `avro:"clientIP"`
+	SessionId string  `avro:"sessionId"`
+	Payload   Payload `avro:"payload"`
+}
+
+type Payload struct {
+	Type     string        `avro:"type"`
+	Target   EventTarget   `avro:"target"`
+	Position EventPosition `avro:"position"`
+}
+
+type EventTarget struct {
+	Type        string `avro:"type"`
+	TagName     string `avro:"tagName"`
+	TextContent string `avro:"textContent"`
+	Link        string `avro:"link"`
+}
+
+type EventPosition struct {
+	PageX int32 `avro:"pageX"`
+	PageY int32 `avro:"pageY"`
+}
+
+var _ = Describe("Test Avro", func() {
+	It("Parse Avro should work", func() {
+		schema, err := ParseAvro(Event{})
+		Expect(err).To(BeNil())
+		Expect(schema).To(Equal(`{"name":"Event","type":"record","fields":[{"name":"ClientIP","type":"string"},{"name":"SessionId","type":"string"},{"name":"Payload","type":"record","fields":[{"name":"Type","type":"string"},{"name":"Target","type":"record","fields":[{"name":"Type","type":"string"},{"name":"TagName","type":"string"},{"name":"TextContent","type":"string"},{"name":"Link","type":"string"}]},{"name":"Position","type":"record","fields":[{"name":"PageX","type":"int"},{"name":"PageY","type":"int"}]}]}]}`))
+	})
+})
